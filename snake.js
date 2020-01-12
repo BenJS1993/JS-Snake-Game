66a195273289d0f25a65f5bcb2205289e67ba354
BenJS1993 <32497552+BenJS1993@users.noreply.github.com>
2020-01-12 22:14:32 +0000

    commit

--- snake.js
+++ snake.js
@@ -1,6 +1,11 @@
 let snake = [ {x: 150, y: 150}, {x: 140, y: 150}, {x: 130, y: 150}, 
     {x: 120, y: 150}, {x: 110, y: 150},];
 
+    dx = +10
+
+let nextSnake = [ {x: 160, y: 150}, {x: 150, y: 150}, {x: 140, y: 150}, 
+    {x: 130, y: 150}, {x: 120, y: 150}]
+
 function drawSnakePart(snakePart) {
     Ctx.fillstyle = 'lightgreen';
     Ctx.strokestyle = 'darkgreen';
@@ -11,3 +16,7 @@ function drawSnakePart(snakePart) {
 function drawSnake() { 
     snake.forEach(drawSnakePart);
 }
+
+function advanceSnake() { const head = {x: snake[0].x + dx, y: snake[0].y}  
+                            snake.unshift(head); 
+                            snake.pop(); }
